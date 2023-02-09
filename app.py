9cb8fbdff03d48cf5b155de062400e5c95b74f91
MarekAniol <marekaniol1@gmail.com>
2023-02-09 09:39:38 +0100

    added missing line to router connection in app file

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from .routers import users
 
 def env_configure():
     load_dotenv()
 
 
 app = FastAPI()
+
+
+app.include_router(users.router)
 
 
 mock_users_db = {
