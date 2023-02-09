bb29df82f474a20edbffda5caa3db7936e9b04f4
MarekAniol <marekaniol1@gmail.com>
2023-02-09 13:21:44 +0100

    added password hashing and JWT tokens

--- auth_handler.py
+++ auth_handler.py
@@ -1,40 +1,73 @@
+from datetime import datetime, timedelta
 from fastapi import HTTPException, Depends, status
 from fastapi.security import HTTPBearer, OAuth2PasswordBearer
 from passlib.context import CryptContext
 import fake_db
-import models.user_models as model
+from models import token_models as t_model, user_models as u_model
+from jose import JWTError, jwt
+import os
 
 
 class AuthHandler():
     security = HTTPBearer()
     pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
     oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+    SECRET_KEY = os.getenv('JWT_SECRET')
+    ALGORITHM = os.getenv('JWT_ALGORITHM')
     
     
+    def verify_password(self, plain_password, hashed_password):
+        return self.pwd_context.verify(plain_password, hashed_password)
+
+    def get_password_hash(self, password):
+        return self.pwd_context.hash(password)
     
-    def fake_decode_token(self, token):
-        user = self.get_user(fake_db.mock_users_db, token)
-        return user
-    
-    async def get_current_user(self, token: str = Depends(oauth2_scheme)):
-        user = self.fake_decode_token(token)
+    def authenticate_user(self, fake_db, username: str, password: str):
+        user = self.get_user(fake_db, username)
         if not user:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid authentication credentials",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
+            return False
+        if not self.verify_password(password, user.hashed_password):
+            return False
         return user
     
-    def fake_hash_password(password: str):
-        return "fakehashed" + password
+    def create_access_token(self, data: dict, expires_delta: timedelta | None = None):
+        to_encode = data.copy()
+        if expires_delta:
+            expire = datetime.utcnow() + expires_delta
+        else:
+            expire = datetime.utcnow() + timedelta(minutes=15)
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(
+            to_encode, self.SECRET_KEY,
+            algorithm=self.ALGORITHM
+        )
+        return encoded_jwt
+
+    async def get_current_user(self, token: str = Depends(oauth2_scheme)):
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+        try:
+            payload = jwt.decode(token, self.SECRET_KEY, algorithms=self.ALGORITHM)
+            username: str = payload.get("sub")
+            if username is None:
+                raise credentials_exception
+            token_data = t_model.TokenData(username=username)
+        except JWTError:
+            raise credentials_exception
+        user = self.get_user(fake_db.mock_users_db, username=token_data.username)
+        if user is None:
+            raise credentials_exception
+        return user
     
     def get_user(db, username: str):
         if username in db: 
             user_dict = db[username]
-            return model.UserInDB(**user_dict)
+            return u_model.UserInDB(**user_dict)
     
-    async def get_current_active_user(current_user: model.User = Depends(get_current_user)):
+    async def get_current_active_user(current_user: u_model.User = Depends(get_current_user)):
         if current_user.disabled:
             raise HTTPException(status_code=400, detail="Inactive user")
         return current_user
--- routers/users.py
+++ routers/users.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException
-import models.user_models as user_model
+from fastapi import APIRouter, Depends, HTTPException, status
+from models import user_models as u_model, token_models as t_model
 from auth_handler import AuthHandler
 from fastapi.security import OAuth2PasswordRequestForm
 import fake_db
+import os
 
 
 router = APIRouter()
@@ -11,20 +12,24 @@ auth_handler = AuthHandler()
 
     
 @router.get("/users/me")
-async def read_users_me(current_user: user_model.User = Depends(auth_handler.get_current_user)):
+async def read_users_me(current_user: u_model.User = Depends(auth_handler.get_current_user)):
     return current_user
 
+@router.get("/users/me/items/")
+async def read_own_items(current_user: u_model.User = Depends(auth_handler.get_current_active_user)):
+    return [{"item_id": "1", "owner": current_user.username}]
 
-@router.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user_dict = fake_db.mock_users_db.get(form_data.username)
-    
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = user_model.UserInDB(**user_dict)
-    hashed_password = AuthHandler.fake_hash_password(form_data.password)
-    
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-
-    return {"access_token": user.username, "token_type": "bearer"}
+@router.post("/token", response_model=t_model.Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = auth_handler.authenticate_user(fake_db.mock_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = auth_handler.timedelta(minutes=os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
+    access_token = auth_handler.create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
